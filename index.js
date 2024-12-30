d088969d2a3d4fa47d7009de6a929ab6131d9a7c
waltershadow <jrescalanteaviles@gmail.com>
2024-12-29 20:46:32 -0600

    Enemy
    se agrego al enemigo en pocisiones random

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as PIXI from "pixi.js";
-//import Victor from "victor";
+import Victor from "victor";
 //import Matter from "matter-js";
 
 let canvasSize = 256;
@@ -20,11 +20,46 @@ square.tint = 0xea985d;
 
 app.stage.addChild(square);
 
+let enemyRadius = 16;
+const enemy = new PIXI.Graphics();
+let r = randomSpawnPoint();
+enemy.position.set(r.x, r.y);
+enemy.beginFill(0xff0000, 1);
+enemy.drawCircle(0, 0, enemyRadius);
+enemy.endFill();
+app.stage.addChild(enemy);
+
 app.ticker.add((delta) => {
-  const cursorPosition = app.renderer.plugins.interaction.mouse.globla;
-  let angle = Math.atan2(cursorPosition.y - square.position.y
-    ,cursorPosition.x - square.position.x 
-  ) + 
-  Math.PI / 2;
+  const cursorPosition = app.renderer.plugins.interaction.mouse.global;
+  let angle =
+    Math.atan2(
+      cursorPosition.y - square.position.y,
+      cursorPosition.x - square.position.x
+    ) +
+    Math.PI / 2;
   square.rotation = angle;
-})
+});
+
+function randomSpawnPoint() {
+  let edge = Math.floor(Math.random() * 4);
+  let spawnPoint = new Victor(0, 0);
+  switch (edge) {
+    case 0:
+      spawnPoint.x = canvasSize * Math.random();
+      break;
+    case 1:
+      spawnPoint.x = canvasSize;
+      spawnPoint, (y = canvasSize * Math.random());
+      break;
+    case 2:
+      spawnPoint.x = canvasSize * Math.random();
+      spawnPoint.y = canvasSize;
+      break;
+    default:
+      spawnPoint.x = 0;
+      spawnPoint.y = canvasSize * Math.random();
+      break;
+  }
+  return spawnPoint;
+}
+
