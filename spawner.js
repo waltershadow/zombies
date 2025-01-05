1ee018926e375ff783fb3619fc190522d10f0ee9
waltershadow <jrescalanteaviles@gmail.com>
2025-01-05 12:59:14 -0600

    creacion del spawner

---
+++ spawner.js
@@ -0,0 +1,15 @@
+export default class Spawner {
+    constructor({ create }) {
+      const spawnInterval = 100;
+      this.maxSpawns = 30;
+      this.create = create;
+      this.spawns = [];
+      setInterval(() => this.spawn(), spawnInterval);
+    }
+    spawn() {
+      if (this.spawns.length >= this.maxSpawns) return;
+      let s = this.create();
+      this.spawns.push(s);
+    }
+  }
+  
