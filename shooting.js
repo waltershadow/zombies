6d46b134774979ff8cf04c3f92228758509471af
waltershadow <jrescalanteaviles@gmail.com>
2025-01-06 20:26:48 -0600

    update shooting

--- shooting.js
+++ shooting.js
@@ -12,6 +12,18 @@ export default class Shooting {
   }
 
   fire() {
+    if (this.bullets.length >= this.maxBullets) {
+      let b = this.bullets.shift();
+      this.app.stage.removeChild(b);
+    }
+    this.bullets.forEach((b) => this.app.stage.removeChild(b));
+    this.bullets = this.bullets.filter(
+      (b) =>
+        Math.abs(b.position.x) < this.app.screen.width &&
+        Math.abs(b.position.y) < this.app.screen.height
+    );
+    this.bullets.forEach((b) => this.app.stage.addChild(b));
+
     const bullet = new PIXI.Graphics();
     bullet.position.set(this.player.position.x, this.player.position.y);
     bullet.beginFill(0x0000ff, 1);
@@ -25,6 +37,7 @@ export default class Shooting {
     ).multiplyScalar(this.bulletSpeed);
     this.bullets.push(bullet);
     this.app.stage.addChild(bullet);
+    console.log(this.bullets.length, this.app.stage.children.length);
   }
 
   set shoot(Shooting) {
