3cc66364ef3bb54c6403dd4b0413b3cced2576a2
waltershadow <jrescalanteaviles@gmail.com>
2025-01-06 20:26:10 -0600

    modificacion index
    se pueden matar los zombies ahora

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as PIXI from "pixi.js";
 import Player from "./player";
 import Zombie from "./zombie";
 import Spawner from "./spawner";
-import Matter from "matter-js";
+//import Matter from "matter-js";
 
 const canvasSize = 512;
 const canvas = document.getElementById("mycanvas");
@@ -19,4 +19,25 @@ let zSpawner = new Spawner({ create: () => new Zombie({ app, player }) });
 app.ticker.add((delta) => {
   player.update();
   zSpawner.spawns.forEach((zombie) => zombie.update());
+  bulletHitTest({
+    bullets: player.shooting.bullets,
+    zombies: zSpawner.spawns,
+    bulletRadius: 8,
+    zombieRadius: 16,
+  });
 });
+
+function bulletHitTest({ bullets, zombies, bulletRadius, zombieRadius }) {
+  bullets.forEach((bullet) => {
+    zombies.forEach((zombie, index) => {
+      let dx = zombie.position.x - bullet.position.x;
+      let dy = zombie.position.y - bullet.position.y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+      if (distance < bulletRadius + zombieRadius) {
+        zombies.splice(index, 1);
+        zombie.kill();
+      }
+    });
+  });
+}
+
